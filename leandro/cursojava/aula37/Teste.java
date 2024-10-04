beb0f095eee9e46327a49e4dbedd376b3ed95570
LeandroDSSF <leandro.hafmaster@gmail.com>
2024-10-04 09:39:08 -0300

    Aula 37 & 38

---
+++ leandro/cursojava/aula37/Teste.java
@@ -0,0 +1,19 @@
+package leandro.cursojava.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+        professor.setSalario("5,000.00");
+
+        Pessoa aluno = new Aluno();
+
+        aluno.setNome("Maria");
+
+        System.out.println(aluno.getNome());
+        System.out.println(professor.getSalario());
+
+    }
+
+}
---
+++ leandro/cursojava/aula38/Aluno.java
@@ -0,0 +1,47 @@
+package leandro.cursojava.aula38;
+
+public class Aluno extends Pessoa {
+
+    private String cursos;
+    private String notas;
+
+    Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String notas, String cursos) {
+        super(nome, endereco, telefone);
+        this.cursos = cursos;
+    }
+
+    public String getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia() {
+        return 0;
+
+    }
+
+    public double verificarAprovado() {
+        return 0;
+    }
+
+    public void metodoQualquer() {
+        super.setCpf("1541551");
+        this.setCpf("949416");
+    }
+
+}
